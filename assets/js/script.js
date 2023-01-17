b38e3cd416c90c241a3b081d9237195ddf9e5d93
IvanGts <navibsilva@gmail.com>
2023-01-17 19:08:32 -0300

    Adicionei os arquivos html, css e js e iniciei o projeto.

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+/* <div class="divInputs">
+    <span class="subTitulos">Escolha um número para deslocamento</span>
+
+    <input type="number" name="numero" id="num" oninput="Num()" min="0" class="inputs">
+</div> */
+
+
+const inputEntrada = document.querySelector('#entrada')
+
+function ativa(){
+    var container = document.createElement('div')
+    container.setAttribute('class', 'divInputs')
+
+    var spanSubTit = document.createElement('span')
+    spanSubTit.setAttribute('class', 'subTitulos')
+
+    var label = document.createElement('label')
+    label.setAttribute('class')
+    var subTitulo = document.createTextNode('Escolha um número para deslocamento.')
+    
+    console.log('funfando')
+}
+
+
+
+// function cifraCesar(){
+//     const container
+// }
