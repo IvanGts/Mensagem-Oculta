ecbcc99f91a5b237976915e18e18a86f5ff6ad3a
IvanGts <navibsilva@gmail.com>
2023-01-17 23:35:21 -0300

    Criei a função que adiciona e retira o bloco de incremento.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,56 @@
 
 
 const inputEntrada = document.querySelector('#entrada')
+const checkCifraCesar = document.querySelector('#cifraCesar')
+const checkBase64 = document.querySelector('#base64')
 
-function ativa(){
-    var container = document.createElement('div')
-    container.setAttribute('class', 'divInputs')
+function escolheCodificacao(){
+    if(checkCifraCesar.checked){
+        ativaCifradeCesar()
+    }else if(checkBase64.checked){
+        ativaBase64()
+    }
+}
 
-    var spanSubTit = document.createElement('span')
-    spanSubTit.setAttribute('class', 'subTitulos')
+escolheCodificacao()
 
-    var label = document.createElement('label')
-    label.setAttribute('class')
-    var subTitulo = document.createTextNode('Escolha um número para deslocamento.')
-    
-    console.log('funfando')
+function ativaCifradeCesar(){
+    if(checkCifraCesar.checked){
+        noneCifraCesar() // retira toda label de dentro da div cifra de cesar
+    }else{
+        criaBloco()
+    }
+}
+
+function ativaBase64(){
+    if(checkCifraCesar.checked){
+        noneCifraCesar()
+    }else{
+        //executa bloco de códificação ou decodificação de acordo com o selecionado
+    }
 }
 
 
+function criaBloco(){
+    var container = document.querySelector('#divCifraCesar')
+    var label = document.createElement('label')     // 
+    label.setAttribute('class', 'labelAcresc')      // 
+    var spanSubTit = document.createElement('span')     // 
+    spanSubTit.setAttribute('class', 'subTitulos')
+    var subTitulo = document.createTextNode('Escolha um número para deslocamento.')
+    spanSubTit.appendChild(subTitulo)
 
-// function cifraCesar(){
-//     const container
-// }
+    var inputAcrescenta = document.createElement('input')
+    inputAcrescenta.setAttribute('class', 'inputAcresc')
+    inputAcrescenta.setAttribute('id', 'acrescenta')
+
+    container.appendChild(label)
+    label.appendChild(spanSubTit)
+    spanSubTit.appendChild(subTitulo)
+    label.appendChild(inputAcrescenta)
+} // função que cria o bloco de incremento que ativada quando clica em Cifra de Cesar
+
+function noneCifraCesar(){
+    var cifraCesarNone = document.querySelector('.labelAcresc')
+    cifraCesarNone.setAttribute('class', 'displayNone')
+}// função que tira a o bloco
